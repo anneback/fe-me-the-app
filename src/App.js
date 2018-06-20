60720b52a258b1bbbe51e7c293593547d35df70a
Jesper Annebäck <jesper.anneback@nordnet.se>
2018-06-20 11:27:30 +0200

    added new header icon, added api JSON object

--- src/App.js
+++ src/App.js
@@ -2,17 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { CountriesList } from './components/countries-list';
+
+const countries = [
+  {
+    currencies: [
+      {
+        code: 'USD',
+        name: 'United States dollar',
+        symbol: '$'
+      }
+    ],
+    flag: 'https://restcountries.eu/data/mhl.svg',
+    name: 'Marshall Islands',
+    alpha2Code: 'MH',
+    capital: 'Majuro'
+  },
+  {
+    currencies: [
+      {
+        code: 'XPF',
+        name: 'CFP franc',
+        symbol: 'Fr'
+      }
+    ],
+    flag: 'https://restcountries.eu/data/wlf.svg',
+    name: 'Wallis and Futuna',
+    alpha2Code: 'WF',
+    capital: 'Mata-Utu'
+  },
+  {
+    currencies: [
+      {
+        code: 'SEK',
+        name: 'Swedish krona',
+        symbol: 'kr'
+      }
+    ],
+    flag: 'https://restcountries.eu/data/swe.svg',
+    name: 'Sweden',
+    alpha2Code: 'SE',
+    capital: 'Stockholm'
+  }
+];
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <CountriesList countries={countries} />
       </div>
     );
   }
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-// export { default as Header } from './header';
-// export * from './content';
+// export { default as Header } from './header'; // if connected component
+export * from './country';
+export * from './countries-list';
